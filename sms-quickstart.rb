339a7e395790d186411fe376a6f11d599261b1a1
Gallagher Polyn <gallagher.polyn@gmail.com>
2016-12-21 15:23:56 -0500

    completed twilio.com/docs/quickstart/ruby/sms/replying-to-sms-messages

---
+++ sms-quickstart.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'sinatra'
+
+get '/sms-quickstart' do
+  sender = params[:From]
+  friends = {
+    "+12067785747" => "Curious George",
+    "+14158157775" => "Boots",
+    "+14155551234" => "Virgil"
+  }
+  name = friends[sender] || "Mobile Monkey"
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hello, #{name}. Thanks for the message."
+  end
+  twiml.text
+end
