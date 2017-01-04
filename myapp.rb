bd13c909927b31b4b4dd3657e4302c30d6320b47
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-01-03 19:47:18 -0500

    conform app to suit rackup style

---
+++ myapp.rb
@@ -0,0 +1,18 @@
+require "rubygems"
+require 'twilio-ruby'
+require "sinatra/base"
+
+class MyApp < Sinatra::Base
+
+  get '/sms-quickstart' do
+    sender = params[:From]
+    friends = {}
+    friends[ENV['MY_FRIENDS_NUMBER']] = "Curious George"
+    name = friends[sender] || "Person I don't yet know"
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hello, #{name}. Thanks for the message."
+    end
+    twiml.text
+  end
+
+end
