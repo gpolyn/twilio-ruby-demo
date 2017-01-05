5d467fef5c0ad7cb038a2cee8525ed3179b49fb7
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-01-05 17:43:07 -0500

    added back in literal tel #

--- myapp.rb
+++ myapp.rb
@@ -7,7 +7,8 @@ class MyApp < Sinatra::Base
   get '/sms-quickstart' do
     sender = params[:From]
     friends = {}
-    friends[ENV['MY_FRIENDS_NUMBER']] = "Curious George"
+    friends["+12067785747"] = "Curious George"
+    # friends[ENV['MY_FRIENDS_NUMBER']] = "Curious George"
     name = friends[sender] || "Person I don't yet know"
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message "Hello, #{name}. Thanks for the message."
